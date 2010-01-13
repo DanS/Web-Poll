df6db776f0e7701d856464a401bc127f141cdb90
Dan Steinicke <dansteinicke@gmail.com>
2010-01-13 10:06:04 -0800

    Fixed arranging of radio buttons depenending on length of associated text, removed some stuff from version control, made a slight change to about message.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+
+  def button_arrangement(question)
+    #determine if radio buttons should be vertical or horizontal
+    length_of_answers = question.answer_choices.split('|').sum {|a| a.length + 4 }
+    if (question.text.length + length_of_answers)  > 60
+      return 'vertical_buttons'
+    else
+      return 'horizontal_buttons'
+    end
+  end
+  
 end
